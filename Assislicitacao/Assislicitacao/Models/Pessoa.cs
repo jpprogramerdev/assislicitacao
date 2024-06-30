4dc972895a994aa9086adfeda24d131de2ec98ec
jpprogramerdev <jpgertto.1204fernadesz@gmail.com>
2024-06-30 20:41:47 -0300

    Criado a classe Pessoa

---
+++ Assislicitacao/Assislicitacao/Models/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace Assislicitacao.Models {
+    public class Pessoa {
+        public string Nome{get;set;}
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Email { get; set; }
+    }
+}
